934ee898b9620a601f05f28b6f5d8c724bc40b4c
Thiago Fraga <fraga.thiagoribeiro@gmail.com>
2024-01-05 15:28:50 -0300

    First commit

---
+++ server/main.js
@@ -0,0 +1,49 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from '/imports/db/TasksCollection';
+import '/imports/api/tasksMethods';
+import '/imports/api/tasksPublications';
+
+const insertTask = (taskText, user) =>
+  TasksCollection.insert({
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(),
+  });
+
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+
+Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
+  ServiceConfiguration.configurations.upsert(
+    { service: 'github' },
+    {
+      $set: {
+        loginStyle: 'popup',
+        clientId: 'baec064e36eac051505e', // insert your clientId here
+        secret: 'd757a306447b399f48e28c88cf92fac1ba23fe08', // insert your secret here
+      },
+    }
+  );
+
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
+
+  if (TasksCollection.find().count() === 0) {
+    [
+      'First Task',
+      'Second Task',
+      'Third Task',
+      'Fourth Task',
+      'Fifth Task',
+      'Sixth Task',
+      'Seventh Task',
+    ].forEach(taskText => insertTask(taskText, user));
+  }
+});
